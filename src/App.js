25eeb9ecc010d0472d0b3b7a33fbe2a0ad485aaa
Nahid1911 <nahid.raihan@gmail.com>
2023-11-16 18:19:44 +0600

    Change Config.js and remove input.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello TailwindCSS from React</h1>
+      <h1 className='italic text-red-500 font-extrabold hover:text-green-500'>Hello TailwindCSS from React</h1>
     </div>
   );
 }
