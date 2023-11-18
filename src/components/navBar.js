e1e40bccc23fc2c9722018ca9500ad067f7341b6
Nahid1911 <nahid.raihan@gmail.com>
2023-11-18 13:25:15 +0600

    Add menu to navbar element

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,9 +3,21 @@ export default function NavBar() {
   return (
     <div className="flex flex-row justify-between p-4 text-blue-100 text-xl bg-blue-950">
       <div>Logo</div>
-      <div>
-        <ul>TailwindCSS</ul>
-      </div>
+      <Menu as="div" className="relative">
+        <div>
+          <Menu.Button>
+            <span>
+              TailwindCSS
+            </span>
+          </Menu.Button>
+        </div>
+        <div>
+          <Menu.Items className="absolute w-40 left-0 bg-slate-500 rounded-lg text-green-200 text-left text-lg p-2 border">
+            <Menu.Item className="block border rounded p-1"><a href=" ">Screen Size</a></Menu.Item>
+            <Menu.Item className="block border rounded p-1 mt-1"><a href=" ">Border</a></Menu.Item>
+          </Menu.Items>
+        </div>
+      </Menu>
       <div className="flex flex-row gap-4">
         <button className="border rounded-full">noti</button>
         <Menu as="div" className="relative border rounded-full">
@@ -14,17 +26,17 @@ export default function NavBar() {
               <span className="relative">Prof</span>
             </Menu.Button>
           </div>
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 rounded-xl bg-blue-900">
-              <Menu.Item className="block border-r-amber-600">
-                <a href=" " className="">Head-1</a>
-              </Menu.Item>
-              <Menu.Item className="block border-r-amber-600">
-                <a href=" ">Head-2</a>
-              </Menu.Item>
-              <Menu.Item className="block border-r-amber-600">
-                <a href=" ">Head-3</a>
-              </Menu.Item>
-            </Menu.Items>
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 rounded-xl bg-blue-900">
+            <Menu.Item className="block border-r-amber-600">
+              <a href=" " className="">Head-1</a>
+            </Menu.Item>
+            <Menu.Item className="block border-r-amber-600">
+              <a href=" ">Head-2</a>
+            </Menu.Item>
+            <Menu.Item className="block border-r-amber-600">
+              <a href=" ">Head-3</a>
+            </Menu.Item>
+          </Menu.Items>
         </Menu>
       </div>
     </div>
