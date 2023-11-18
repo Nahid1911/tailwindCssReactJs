86862dbd6f13032723f6da40df8c0eb5287a29fa
Nahid1911 <nahid.raihan@gmail.com>
2023-11-18 20:42:35 +0600

    Adjust the styling of navBar and ScreenSize

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,7 +3,7 @@ import { Menu } from "@headlessui/react";
 export default function NavBar() {
   return (
     <>
-    <div className="flex flex-row justify-between p-4 text-blue-100 text-xl bg-blue-950">
+    <div className="flex flex-row w-full justify-between p-4 text-blue-100 text-xl bg-blue-950">
       <div>Logo</div>
       <Menu as="div" className="relative">
         <div>
@@ -14,7 +14,7 @@ export default function NavBar() {
           </Menu.Button>
         </div>
         <div>
-          <Menu.Items className="absolute w-40 left-0 bg-slate-500 rounded-lg text-green-200 text-left text-lg p-2 border">
+          <Menu.Items className="absolute w-40 left-0 bg-slate-500 rounded-lg text-green-200 text-left text-sm mt-1 p-2 border">
             <Menu.Item className="block border rounded p-1"><Link to="/">Screen Size</Link></Menu.Item>
             <Menu.Item className="block border rounded p-1 mt-1"><Link to="/dropdown">Tailwind Dropdown</Link></Menu.Item>
           </Menu.Items>
--- src/components/screenSize.js
+++ src/components/screenSize.js
@@ -1,11 +1,11 @@
 function ScreenSize () {
   return (
-    <div>
+    <div className="text-center w-full">
     <h1 className='italic mt-8 text-red-500 font-extrabold text-2xl text-center'>TailwindCSS Screen Size Visualization</h1>
     <p className="text-center">This is a simple example of how to use Tailwind CSS's screen size classes</p>
     <p className="text-center">Resize your browser window to see the effect</p>
     <p className="text-center">The background color changes at the following breakpoints:<span className="font-bold text-lg text-red-400"> sm, md, lg, xl, 2xl</span></p>
-    <div className=" flex m-4 h-[300px] font-bold justify-between text-white bg-yellow-600 sm:bg-red-900 md:bg-green-900 lg:bg-blue-800 xl:bg-violet-500 2xl:bg-orange-700">
+    <div className=" flex flex-row m-4 h-auto font-bold justify-between text-white bg-yellow-600 sm:bg-red-900 md:bg-green-900 lg:bg-blue-800 xl:bg-violet-500 2xl:bg-orange-700">
       <div className="flex w-auto h-auto p-1 m-4 items-center text-2xl bg-red-900 sm:max-md:text-green-500 sm:max-md:border">sm: 640px <br/> (min-width)</div>
       <div className="flex w-auto h-auto p-1 m-4 items-center text-2xl bg-green-900 md:max-lg:text-green-500 md:max-lg:border">md: 768px <br/> (min-width)</div>
       <div className="flex w-auto h-auto p-1 m-4 items-center text-2xl bg-blue-800 lg:max-xl:text-green-500 lg:max-xl:border">lg: 1024px <br/> (min-width)</div>
