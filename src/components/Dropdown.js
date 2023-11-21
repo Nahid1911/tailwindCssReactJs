e37d3647887df2d0bef8125504bbebaedb63559e
Nahid1911 <nahid.raihan@gmail.com>
2023-11-21 21:56:15 +0600

    Create button for dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,12 @@
+import { Menu, Transition } from "@headlessui/react";
 export default function Dropdown() {
   return(
-    <h1>MenuTutorial</h1>
+    <div className="w-full text-center pt-40">
+      <Menu as="div" className="relative">
+        <div>
+        <Menu.Button className="border p-4 bg-slate-300 hover:bg-slate-950 hover:text-white rounded-full">Dropdown</Menu.Button>
+        </div>
+      </Menu>
+    </div>
   )
 }
