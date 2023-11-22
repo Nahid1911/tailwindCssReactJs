321fc697246dd5191981bd22caf54cec52f01392
Nahid1911 <nahid.raihan@gmail.com>
2023-11-22 20:14:04 +0600

    Add menu items for dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,23 @@
 import { Menu, Transition } from "@headlessui/react";
 export default function Dropdown() {
-  return(
+  return (
     <div className="w-full text-center pt-40">
       <Menu as="div" className="relative">
         <div>
-        <Menu.Button className="border p-4 bg-slate-300 hover:bg-slate-950 hover:text-white rounded-full">Dropdown</Menu.Button>
+          <Menu.Button className="border p-4 bg-slate-300 hover:bg-slate-950 hover:text-white rounded-full">
+            <span>
+              Dropdown
+            </span>
+          </Menu.Button>
         </div>
+        <Menu.Items>
+          <Menu.Item>Dropdown List-1</Menu.Item>
+          <Menu.Item>Dropdown List-2</Menu.Item>
+          <Menu.Item>Dropdown List-3</Menu.Item>
+          <Menu.Item>Dropdown List-4</Menu.Item>
+          <Menu.Item>Dropdown List-5</Menu.Item>
+          <Menu.Item>Dropdown List-6</Menu.Item>
+        </Menu.Items>
       </Menu>
     </div>
   )
